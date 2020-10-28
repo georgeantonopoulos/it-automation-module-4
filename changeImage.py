60c99068ec01d923566855c75af483d3106102dd
George Antonopoulos <geeobcr@gmail.com>
2020-10-28 14:58:14 +0000

    Final used in Module 4 QKlabs Changed png to tiff Added resizing removed rotation corrected image directory :

--- changeImage.py
+++ changeImage.py
@@ -4,10 +4,9 @@ import os, sys
 from PIL import Image
 
 home_dir = os.path.expanduser("~")
-img_dir = r"/home/geeobcr/convert_images/images"
+img_dir = r"{0}/supplier-data/images/".format(home_dir)
 output_dir = r"{0}/supplier-data/images".format(home_dir)
 size = (600,400)
-rotation = -90
 
 # Create Directory if doesn't exist
 if not os.path.exists(output_dir):
@@ -15,18 +14,17 @@ if not os.path.exists(output_dir):
 
 # Process all files in img_dir
 for f in os.listdir(img_dir):
-    if not f.startswith("."):
+    if not f.startswith(".") and "." in f:
         filename = None
-        if ".png" in f:
-            filename = f.replace(".png",".jpeg")
-        else:
-            filename = f + ".jpeg"
+        if ".tiff" in f:
+            filename = f.replace(".tiff",".jpeg")
+        
         if filename:
             outfile = os.path.join(output_dir, filename)
 
         try:
             img = Image.open(os.path.join(img_dir,f))
-            #img = img.rotate(rotation).resize(size)
+            img = img.resize(size)
             img.convert('RGB').save(outfile, "JPEG")
         except OSError:
             print("unable to convert file", f)
