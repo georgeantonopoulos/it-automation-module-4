f2ad8428c33ad83bebe12e5ec68cbe66b0348f33
George Antonopoulos <geeobcr@gmail.com>
2020-10-29 09:26:32 +0000

    Adds missing slash in url

--- supplier_image_upload.py
+++ supplier_image_upload.py
@@ -4,9 +4,11 @@ import os
 
 home_dir = os.path.expanduser("~")
 img_dir = r"{0}/supplier-data/images".format(home_dir)
-url = "http://localhost/upload"
+url = "http://localhost/upload/"
 
 for image in os.listdir(img_dir):
-    image_path = os.path.join(img_dir,image)
-    with open(image_path, 'rb') as img_opened:
-        r = requests.post(url, files={'file':img_opened})
+    if image.endswith(".jpeg"):
+        image_path = os.path.join(img_dir,image)
+        with open(image_path, 'rb') as img_opened:
+            r = requests.post(url, files={'file':img_opened})
+            print(r.status_code, r.text)
